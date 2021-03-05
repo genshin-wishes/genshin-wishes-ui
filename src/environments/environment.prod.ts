6dcb6424592fb026210aaf193d04c59854146dc1
Ishou <Ishou@users.noreply.github.com>
2021-03-05 16:01:01 +0100

    fix(1.4.4): hot fix 1.4.3 issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,8 +6,5 @@ export const environment = {
     fr: 'https://www.youtube.com/watch?v=uObSZ6Dz2Hw',
     en: 'https://www.youtube.com/watch?v=a16X0R_rSZc',
   },
-  demo: {
-    fr: 'https://www.youtube.com/embed/uObSZ6Dz2Hw?playlist=uObSZ6Dz2Hw',
-    en: 'https://www.youtube.com/embed/uObSZ6Dz2Hw?playlist=uObSZ6Dz2Hw',
-  },
+  demo: 'https://www.youtube.com/embed/twnZ-gppAWA?playlist=twnZ-gppAWA',
 };
