3b718d58279b3ceb7150b94ddacb6c85df83a08e
Ishou <Ishou@users.noreply.github.com>
2021-04-06 04:10:25 +0200

    fix(pity-chip): use right color for weapon banner (pity is 10 less)

--- src/app/gw-app/stats/five-stars-list/five-stars-history/five-stars-history.component.ts
+++ src/app/gw-app/stats/five-stars-list/five-stars-history/five-stars-history.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { FiveStarDetail } from '../five-stars-list.component';
 import { Wish } from '../../../../api/genshin-wishes/wish';
+import {
+  BannerToId,
+  BannerType,
+  IdToBanner,
+  PITY_5_BY_TYPE,
+} from '../../../../api/genshin-wishes/constants';
 
 @Component({
   selector: 'app-five-stars-history',
@@ -10,6 +16,12 @@ import { Wish } from '../../../../api/genshin-wishes/wish';
 export class FiveStarsHistoryComponent {
   @Input()
   detail!: FiveStarDetail;
+
+  PITY_5_BY_TYPE = PITY_5_BY_TYPE;
+
+  IdToBanner = IdToBanner;
+
+  permanentPity = PITY_5_BY_TYPE[BannerType.CHARACTER_EVENT];
 
   isFocused(wish: Wish): boolean {
     return !!wish.banner?.items.find((i) => i.itemId === wish.item?.itemId);
--- src/app/shared/pity-chip/pity-chip.component.ts
+++ src/app/shared/pity-chip/pity-chip.component.ts
@@ -7,5 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class PityChipComponent {
   @Input()
+  harmonizedPity!: number;
+  @Input()
   pity!: number;
 }
