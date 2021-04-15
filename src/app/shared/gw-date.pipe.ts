9a285329bee63edb6a637d875001daffafbe98d4
Ishou <Ishou@users.noreply.github.com>
2021-04-15 23:40:22 +0200

    fix(i18n): use locale time format if available for 12h format

--- src/app/shared/gw-date.pipe.ts
+++ src/app/shared/gw-date.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { LangService } from './lang.service';
-import { DatePipe } from '@angular/common';
+import { DatePipe, FormatWidth, getLocaleTimeFormat } from '@angular/common';
 import { AuthService } from '../auth/auth.service';
 
 @Pipe({
@@ -14,13 +14,20 @@ export class GwDatePipe implements PipeTransform {
     format?: string,
     timezone?: string
   ): string | null {
-    const datePipe = new DatePipe(this._lang.getCurrentLang());
+    const currentLang = this._lang.getCurrentLang();
+    const datePipe = new DatePipe(currentLang);
+    const localeMediumTime = getLocaleTimeFormat(
+      currentLang,
+      FormatWidth.Short
+    );
 
     format =
       format === 'shortTime' &&
       this._auth.getCurrentUser()?.wholeClock != undefined
         ? this._auth.getCurrentUser()?.wholeClock
           ? 'HH:mm'
+          : localeMediumTime.includes('a')
+          ? localeMediumTime
           : 'h:mm a'
         : format;
 
