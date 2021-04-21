a5ae13e7d3cc97e8d7801afa304a3a0d00f0f378
Ishou <Ishou@users.noreply.github.com>
2021-04-21 18:34:16 +0200

    fix(pity): fix pity color for weapon banner

--- src/app/shared/pity-chip/pity-chip.component.ts
+++ src/app/shared/pity-chip/pity-chip.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Wish } from '../../api/genshin-wishes/wish';
+import {
+  BannerToId,
+  BannerType,
+  BannerTypes,
+} from '../../api/genshin-wishes/constants';
 
 @Component({
   selector: 'app-pity-chip',
@@ -16,8 +21,8 @@ export class PityChipComponent {
     3: { lucky: 0, soft: 0, hard: 0 },
     4: {
       lucky: 3,
-      soft: 6,
-      hard: 9,
+      soft: 9,
+      hard: 10,
     },
     5: {
       lucky: 45,
@@ -25,4 +30,35 @@ export class PityChipComponent {
       hard: 82,
     },
   };
+
+  readonly weaponThresholds = {
+    3: { lucky: 0, soft: 0, hard: 0 },
+    4: {
+      lucky: 3,
+      soft: 8,
+      hard: 10,
+    },
+    5: {
+      lucky: 45,
+      soft: 63,
+      hard: 72,
+    },
+  };
+
+  computeClass(): string {
+    if (!this.wish.item || this.wish.item.rankType < 4) return '';
+
+    const threshold = (this.wish.gachaType ===
+    BannerToId[BannerType.WEAPON_EVENT]
+      ? this.weaponThresholds
+      : this.thresholds)[this.wish.item.rankType];
+
+    return this.wish.pity < threshold.lucky
+      ? ' lucky'
+      : this.wish.pity < threshold.soft
+      ? ' early'
+      : this.wish.pity < threshold.hard
+      ? ' soft'
+      : ' hard';
+  }
 }
