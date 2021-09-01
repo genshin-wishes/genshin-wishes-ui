3d4affbd616e0b2fe45581b33bd512f482cac7c0
Ishou <Ishou@users.noreply.github.com>
2021-09-01 17:34:14 +0200

    feat: get global stats back
    This reverts commit 72444aa3ace5c3a49e7555b171da06c9163188a9.

--- src/app/api/genshin-wishes/import.service.ts
+++ src/app/api/genshin-wishes/import.service.ts
@@ -64,7 +64,7 @@ export class ImportService {
     exhaustMap((scan) =>
       !scan.scan
         ? of(scan.state)
-        : interval(2000).pipe(
+        : interval(1000).pipe(
             exhaustMap(() =>
               this._http.get<ImportResponse>('/api/wishes/importState')
             ),
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,13 @@ const routes: Routes = [
       import('./profile/profile.module').then((m) => m.ProfileModule),
   },
   {
+    path: 'global-stats',
+    loadChildren: () =>
+      import('./public-stats/public-stats.module').then(
+        (m) => m.PublicStatsModule
+      ),
+  },
+  {
     path: '',
     loadChildren: () =>
       import('./landing/landing.module').then((m) => m.LandingModule),
