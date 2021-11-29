60a83da4ab0668b4adace149b44381c845f33fc9
Ishou <Ishou@users.noreply.github.com>
2021-11-29 14:38:04 +0100

    feat: add broken wishes warning

--- src/app/shared/icon/icon.module.ts
+++ src/app/shared/icon/icon.module.ts
@@ -39,6 +39,7 @@ import { faBan } from '@fortawesome/free-solid-svg-icons/faBan';
 import { faChartPie } from '@fortawesome/free-solid-svg-icons/faChartPie';
 import { faLaptopHouse } from '@fortawesome/free-solid-svg-icons/faLaptopHouse';
 import { faPaw } from '@fortawesome/free-solid-svg-icons/faPaw';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons/faExclamationTriangle';
 
 @NgModule({
   exports: [FontAwesomeModule],
@@ -81,7 +82,8 @@ export class IconModule {
       faChartPie,
       faLaptopHouse,
       faTimesCircle,
-      faPaw
+      faPaw,
+      faExclamationTriangle
     );
   }
 }
